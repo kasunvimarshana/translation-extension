08102b7ff844ff1719f98e7c2f8810fb02604342
kasunvimarshana <kasunvmail@gmail.com>
2025-06-23 23:29:19 +0530

    custom browser extension for translation > dev-11

--- content.js
+++
@@ -1,7 +0,0 @@
-// Listen for messages from background script
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "getSelection") {
-    const selection = window.getSelection().toString();
-    sendResponse({ selection: selection });
-  }
-});
