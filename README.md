da52dd4908f6b196a472784a420663ff75851955
kasunvimarshana <kasunvmail@gmail.com>
2025-06-23 22:42:59 +0530

    custom browser extension for translation > dev-08

--- README.md
+++ README.md
@@ -1,15 +1,1 @@
 "# translation-extension"
-
-translation-extension/
-├─ manifest.json
-├─ background.js
-├─ content.js
-├─ translation.js
-├─ icons/
-│  └─ icon.png
-├─ README.md
-
-# Install & Test
-## Save files in a directory (translation-extension).
-
-Open chrome://extensions ➔ Load unpacked ➔ Choose directory.
--- content.js
+++
@@ -1,9 +0,0 @@
-(async () => {
-  const selectedText = window.getSelection().toString().trim();
-  if (!selectedText) return;
-
-  const module = await import(chrome.runtime.getURL('translation.js'));
-  const translation = module.translateText(selectedText);
-
-  alert(`Original : ${selectedText}\nTranslated : ${translation}`);
-})();
